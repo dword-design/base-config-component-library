48269596c9a96b30e0ec8080989714fb4880454b
foo <foo@bar.de>
2020-07-21 03:31:28 +0200

    feat: use rollup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import componentConfig from '@dword-design/base-config-component'
+import execa from 'execa'
+import { outputFile, remove } from 'fs-extra'
+import getPackageName from 'get-package-name'
+import P from 'path'
+
+import entry from './entry'
 
 export default {
   ...componentConfig,
-  packageConfig: {
-    main: 'dist/index.js',
+  commands: {
+    ...componentConfig.commands,
+    prepublishOnly: async () => {
+      try {
+        await outputFile(P.join('src', 'entry.js'), entry)
+        await remove('dist')
+        await execa(
+          getPackageName(require.resolve('rollup')),
+          [
+            '--config',
+            require.resolve('@dword-design/rollup-config-component'),
+          ],
+          {
+            env: { NODE_ENV: 'production', stdio: 'inherit' },
+          }
+        )
+      } finally {
+        await remove(P.join('src', 'entry.js'))
+      }
+    },
   },
 }
